27daf4d9eb85ea631bc5dc32dab92d137af0abb7
GianM <gmagliotti@itba.edu.ar>
2023-04-02 03:07:37 -0300

    Made fetch work

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,8 +5,16 @@ import { Box, Button } from '@chakra-ui/react';
 import { Card, CardHeader, CardBody, CardFooter, Heading, Text } from '@chakra-ui/react'
 import { Progress } from '@chakra-ui/react';
 import { Link } from '@chakra-ui/react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
 import { useState, useEffect } from 'react';
-import piggysData from './piggys.json'
 
 function InfoBox({piggyOwner, currAmount, goalAmount, bgColor}) {
     const piggyBanksLink = "/home/piggyBanks/"
@@ -18,30 +26,35 @@ function InfoBox({piggyOwner, currAmount, goalAmount, bgColor}) {
           <Box w={400}>
             <Text fontSize='16' as='span' textAlign='Left' fontFamily='Helvetica'>Chanchito de {piggyOwner}</Text>
             <Text textAlign='Right' fontSize='20'><b>${currAmount}</b> <Text as='span' fontSize='12'>DE</Text> <b>${goalAmount}</b></Text>
-            <Progress h={6} hasStripe colorScheme='green' value={(currAmount/goalAmount)*100}></Progress>
+            <Progress h={6} borderRadius="md" hasStripe colorScheme='green' value={(currAmount/goalAmount)*100}></Progress>
           </Box>
         </VStack>
       </Box>
       </Link>
     );
 }
+
  
 const Home = () => {
 
   const [piggyList, setPiggyList] = useState([]);
 
   useEffect(() => {
-        const mappedArray = piggysData.map((element) => {
-          return (
-            <InfoBox 
-              piggyOwner={element.piggyOwner}
-              currAmount={element.currentAmount}
-              goalAmount={element.goalAmount}
-              bgColor="gray.100"
-            />
-          );
-        });
+    fetch('/piggys.json')
+      .then(response => response.json())
+      .then(data => {
+        const mappedArray = data.map(item => {
+          // Map the data to the desired format
+          return {
+            piggyOwner: item.piggyOwner,
+            currentAmount: item.currentAmount,
+            goalAmount: item.goalAmount,
+            operations: item.operations
+          };
+        });;
         setPiggyList(mappedArray);
+      })
+      .catch(error => console.error());
   }, []);
 
 
@@ -58,7 +71,10 @@ const Home = () => {
             </Heading>
               
             
-            {piggyList}
+            {piggyList.map((item, index) => (
+              <InfoBox key={index} piggyOwner={item.piggyOwner} currAmount={item.currentAmount} goalAmount={item.goalAmount} bgColor='gray.100'>
+              </InfoBox>
+    ))}
             <Button
               size="lg"
               position="fixed"
